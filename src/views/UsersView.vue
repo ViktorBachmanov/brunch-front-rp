9254c6a7f8eef21035822ebea118540aa95aa20f
Vikt0r <vbachmanov@mail.ru>
2022-07-03 12:24:41 +0300

    Add VITE_API_HOST environment variable

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,5 +1,22 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      users: null,
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const response = await fetch(import.meta.env.VITE_API_HOST + "/site/db");
+      this.users = await response.json();
+    },
+  },
+};
+</script>
 
 <template>
-  <main>Users</main>
+  <main>{{ users }}</main>
 </template>
