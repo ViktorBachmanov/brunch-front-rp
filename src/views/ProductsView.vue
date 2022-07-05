f24f82370da3dd13273034f48b93831e80a60aa3
Vikt0r <vbachmanov@mail.ru>
2022-07-05 12:36:34 +0300

    Create products view table

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -18,5 +18,30 @@ export default {
 </script>
 
 <template>
-  <main>{{ products }}</main>
+  <main>
+    <h3 v-if="!products">Loading...</h3>
+    {{ products }}
+    <table v-if="products">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Наименование</th>
+          <th>Цена</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product['id']">
+          <td>
+            {{ product["id"] }}
+          </td>
+          <td :style="{ textAlign: 'left' }">
+            {{ product["name"] }}
+          </td>
+          <td>
+            {{ product["price"] }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
 </template>
