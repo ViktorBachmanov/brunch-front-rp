9f1825804bc82f585b2c36f726119b8910c71d31
Vikt0r <vbachmanov@mail.ru>
2022-07-05 10:12:00 +0300

    Orginize orders view table via multiple tbodyes

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -14,9 +14,59 @@ export default {
       this.orders = await response.json();
     },
   },
+  computed: {
+    priceTotal() {
+      let sum = 0;
+      this.orders.forEach((order) => {
+        order["products"].forEach((product) => {
+          sum += product["sum"];
+        });
+      });
+      return sum;
+    },
+  },
 };
 </script>
 
 <template>
-  <main>{{ orders }}</main>
+  <main>
+    {{ orders }}
+    <table>
+      <thead>
+        <tr>
+          <th>&#8470;</th>
+          <th>ID клиента</th>
+          <th>Продукт</th>
+          <th>Кол.</th>
+          <th>Итого</th>
+        </tr>
+      </thead>
+      <tbody v-for="order in orders" :key="order['id']">
+        <tr
+          v-for="(product, index) in order['products']"
+          :key="order['products']['name']"
+        >
+          <td :rowspan="order['products'].length" v-if="index === 0">
+            {{ order["id"] }}
+          </td>
+          <td :rowspan="order['products'].length" v-if="index === 0">
+            {{ order["userId"] }}
+          </td>
+          <td :style="{ textAlign: 'left' }">
+            {{ order["products"][index]["name"] }}
+          </td>
+          <td>
+            {{ order["products"][index]["quantity"] }}
+          </td>
+          <td>{{ order["products"][index]["sum"] }}</td>
+        </tr>
+      </tbody>
+      <tbody>
+        <tr>
+          <td colspan="4"></td>
+          <td>{{ priceTotal }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
 </template>
