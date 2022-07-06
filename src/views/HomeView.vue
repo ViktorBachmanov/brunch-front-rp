f6ce99f192e5aaba4c3ff0f571cea375c1e74f60
Vikt0r <vbachmanov@mail.ru>
2022-07-06 09:17:57 +0300

    Wakeup Heroku

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import TheWelcome from "@/components/TheWelcome.vue";
+import { fetchData } from "../js/util";
+
+// Wakeup Heroku
+fetchData("users");
 </script>
 
 <template>
